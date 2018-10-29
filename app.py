3ca081383a28c89486dd12b12273ae8bccc3359e
Nicholas Palenchar <nickpalenchar@users.noreply.github.com>
2018-10-28 20:43:36 -0400

    mock login

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
 
 app = Flask(__name__)
 
+app.secret_key = 'YouWillNeverGuess'
+
+
 @app.route('/')
-def hello():
-    return render_template('new.html')
+def hello():   
+    return render_template('new.html', user=session.get('user', None))
+
+
+@app.route('/mocklogin')
+def mock_login():
+    session['user'] = { 'user': 'null' }
+    return 'you are now logged in for testing purposes'
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/', code=302)
 
 
 if __name__ == '__main__':
